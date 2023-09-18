b295dae252585d4d520399c0e1d10ee924dab4e4
Andrew Copeland <50109276+AndrewCopeland@users.noreply.github.com>
2023-09-17 22:27:40 -0400

    prod stylesheet bumping version to 6 (#5)

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
 var APP_PREFIX = 'foradex_'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_04'              // Version of the off-line cache (change this value everytime you want to update cache)
+var VERSION_NUM = 6
+var VERSION = `version_${VERSION_NUM}`              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 var URLS = [                            // Add URL you want to cache in this list.
   '/foradex/',                     // If you have separate JS/CSS files,
